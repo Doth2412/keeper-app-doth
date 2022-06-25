fd40ff4a23e400403741459ccee0455f475c68b7
Doth2412 <tbnrdongthuc2412@gmail.com>
2022-06-25 11:24:37 +0700

    Part3 with bug
    Somehow can't add note

--- keeper-app/src/components/App.jsx
+++ keeper-app/src/components/App.jsx
@@ -1,22 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './Header'
 import Footer from './Footer'
 import Note from './Note'
-import notes from '../notes'
+import Form from './Form'
 
 function App(){
+    const [listNote, setListNote] = useState([])
+
+    function addNote(newNote){
+        setListNote(prevListNote => {
+            return [...prevListNote, newNote]
+        })
+    }
+
+    function deleteNote(id){
+        setListNote(prevListNote =>{
+            return prevListNote.filter((Item, index) => {
+                return index !== id
+            })
+        })
+    }
+
     return(
         <div>
             <Header/>  
-            {notes.map(Entry => (
-            <Note 
-            key = {Entry.id}
+            <Form onSubmit = {addNote}/>
+            {listNote.map((Entry, index) => {
+                return (<Note 
+            key = {index}
+            id = {index}    
             title = {Entry.title}
             content = {Entry.content}
-            />))}
+            onDelete = {deleteNote}
+            />)
+            }
+            )}
             <Footer />
         </div>
     );
 }
 
-export default App
+export default App;
---
+++ keeper-app/src/components/Form.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+
+function Form(props){
+    const [note, setNote] = useState({
+        title: "",
+        content: ""
+    })
+
+    function handleChange(event) {
+        const { name, value } = event.target;
+        setNote((prevNote) => {
+            return {
+                ...prevNote,
+                [name]: value
+            };
+        });
+    }
+
+    function submit(event){
+        props.onAdd(note)
+        setNote({
+            title: "",
+            content: ""
+        });
+        event.preventDefault()
+    }
+
+    return(
+        <div>
+            <form>
+                <input 
+                    name = "title" 
+                    type = "text" 
+                    placeholder='title' 
+                    onChange={handleChange} 
+                    value={note.title} 
+                /> 
+                <textarea 
+                    name = "content"
+                    placeholder= 'Take a note' 
+                    rows = "3" 
+                    value={note.content}
+                    onChange={handleChange}
+                />
+                <button onClick={submit}>ADD</button>
+            </form>
+        </div>
+    )
+}
+
+export default Form
